5c47ac358bcfb3ceca31d9b19d513489c9b2c3ea
DarkProrok <Seroshtanmakcs@gmail.com>
2023-12-02 06:02:32 +0300

    Add PaymentInfo for product

--- Payment/models.py
+++ Payment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class ItemPaymentInfo(models.Model):
+    choices = [
+        ('usd', 'USD'),
+        ('eur', 'EUR')
+    ]
+
+    price = models.FloatField()
+    currency = models.CharField(max_length=50, choices=choices)
+
+    def __str__(self):
+        return f'{self.product.name} Payment'

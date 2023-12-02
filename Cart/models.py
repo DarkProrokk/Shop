50c1a851a253da88ebe79bcd3f2d9c90d59cefba
DarkProrok <Seroshtanmakcs@gmail.com>
2023-12-02 06:02:12 +0300

    add Cart and CustomUser with autogenerate Cart

--- Cart/models.py
+++ Cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Cart(models.Model):
+    products = models.ManyToManyField('Product.Product', blank=True)
+
+    def __str__(self):
+        return self.user.username
--- User/models.py
+++ User/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from Cart.models import Cart
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE, verbose_name='Корзина', related_name='user')
+
+    def save(self, *args, **kwargs):
+        if not CustomUser.objects.filter(pk=self.pk).exists():
+            self.cart = Cart.objects.create()
+        super().save(*args, **kwargs)

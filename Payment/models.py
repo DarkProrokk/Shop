bccbc5a67cef10293a91ce863ab71173906b2736
DarkProrok <Seroshtanmakcs@gmail.com>
2023-12-03 13:23:29 +0300

    Add payment info for product

--- Payment/models.py
+++ Payment/models.py
@@ -10,8 +10,12 @@ class ItemPaymentInfo(models.Model):
         ('eur', 'EUR')
     ]
 
-    price = models.FloatField()
-    currency = models.CharField(max_length=50, choices=choices)
+    price = models.FloatField(verbose_name="Цена")
+    currency = models.CharField(max_length=50, choices=choices, verbose_name="Валюта")
 
     def __str__(self):
         return f'{self.product.name} Payment'
+
+    class Meta:
+        verbose_name = "Платёжная информация товара"
+        verbose_name_plural = "Платёжные информации товаров"

293330f0b10afaf4386221a30d37524758d3cc11
DarkProrok <Seroshtanmakcs@gmail.com>
2023-12-07 06:45:02 +0300

    Add views and path for user cart

--- Cart/views.py
+++ Cart/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import HttpResponse
-from django.views import View
+from django.shortcuts import render
+from django.views.generic import DetailView
+from django.db import models
+from .models import Cart
 
 
 # Create your views here.
 
 
-class AddItemInCart(View):
+class CartView(DetailView):
+    model = Cart
+    template_name = 'Cart/cart.html'
+    context_object_name = 'cart'
 
-    def post(self, request, pk):
-        print(request, pk)
-        return HttpResponse("Предмет добавлен")
+    def get(self, request, *args, **kwargs):
+        cart = Cart.objects.get(pk=1)
+        return render(request, 'Cart/cart.html', {'cart': cart})
